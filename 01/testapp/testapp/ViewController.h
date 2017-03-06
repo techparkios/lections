d04e7c5027bbd4bcd2154a39ccca16625caa9e08
d.taraev <d.taraev@msk-wire-d_taraev-d.taraev.mail.msk>
2017-03-06 15:10:41 +0300

    добавлен код примера с лекции

---
+++ 01/testapp/testapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testapp
+//
+//  Created by d.taraev on 04.03.17.
+//  Copyright © 2017 mail.ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 01/testapp/testapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testapp
+//
+//  Created by d.taraev on 04.03.17.
+//  Copyright © 2017 mail.ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
